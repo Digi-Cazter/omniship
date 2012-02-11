82f8874ef52fc6a5e8ab0b62e3c91f9a2d35afb5
Donavan White <donavan@lifestrength.com>
2012-02-10 21:55:50 -0700

    Added Shipment Accept code for getting the label

--- lib/omniship/carriers/ups.rb
+++ lib/omniship/carriers/ups.rb
@@ -462,8 +462,8 @@ module Omniship
 
 			if success
 			  @shipment = [] 
-        shipment << :tracking_number => root.elements['ShipmentResults/PackageResults/TrackingNumber'].to_s,
-                    :label => root.elements['ShipmentResults/PackageResults/LabelImage/HTMLImage'].get_text
+        @shipment << :tracking_number => root.elements['ShipmentResults/PackageResults/TrackingNumber'].to_s
+        @shipment << :label => root.elements['ShipmentResults/PackageResults/LabelImage/HTMLImage'].get_text
 			end
 			return @shipment
 		end
