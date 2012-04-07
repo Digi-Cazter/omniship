cdca1c881bf862e0576aad47af816eb3030e200a
Donavan White <digi.cazter@gmail.com>
2012-04-06 20:38:02 -0600

    Debugging code

--- lib/omniship/carriers/ups.rb
+++ lib/omniship/carriers/ups.rb
@@ -120,6 +120,7 @@ module Omniship
       packages = Array(packages)
       access_request = build_access_request
       ship_confirm_request = build_ship_confirm(origin, destination, packages, options)
+			debugger
       response = commit(:shipconfirm, save_request(access_request + ship_confirm_request), (options[:test] || true))
       parse_ship_confirm_response(origin, destination, packages, response, options)
     end
