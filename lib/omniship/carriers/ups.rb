78b3c642c69855b61d4c1c4423301efd62c0a492
Donavan White <digi.cazter@gmail.com>
2012-09-25 21:14:06 -0600

    Debugging UPS rate checking

--- lib/omniship/carriers/ups.rb
+++ lib/omniship/carriers/ups.rb
@@ -375,7 +375,6 @@ module Omniship
     end
 
     def parse_rate_response(origin, destination, packages, response, options={})
-      #TODO
       rates = []
       
       xml = Nokogiri::XML(response)
@@ -385,22 +384,21 @@ module Omniship
       if success
         rate_estimates = []
         
-        #xml.elements.each('/*/RatedShipment') do |rated_shipment|
-        #  service_code = rated_shipment.get_text('Service/Code').to_s
-        #  days_to_delivery = rated_shipment.get_text('GuaranteedDaysToDelivery').to_s.to_i
-        #  delivery_date  = days_to_delivery >= 1 ? days_to_delivery.days.from_now.strftime("%Y-%m-%d") : nil
+        xml.elements.each('/*/RatedShipment') do |rated_shipment|
+          service_code = rated_shipment.get_text('Service/Code').to_s
+          days_to_delivery = rated_shipment.get_text('GuaranteedDaysToDelivery').to_s.to_i
+          delivery_date  = days_to_delivery >= 1 ? days_to_delivery.days.from_now.strftime("%Y-%m-%d") : nil
 
-        #  rate_estimates << RateEstimate.new(origin, destination, @@name,
-        #                      service_name_for(origin, service_code),
-        #                      :total_price => rated_shipment.get_text('TotalCharges/MonetaryValue').to_s.to_f,
-        #                      :currency => rated_shipment.get_text('TotalCharges/CurrencyCode').to_s,
-        #                      :service_code => service_code,
-        #                      :packages => packages,
-        #                      :delivery_range => [delivery_date])
-        #end
+          rate_estimates << RateEstimate.new(origin, destination, @@name,
+                              service_name_for(origin, service_code),
+                              :total_price => rated_shipment.get_text('TotalCharges/MonetaryValue').to_s.to_f,
+                              :currency => rated_shipment.get_text('TotalCharges/CurrencyCode').to_s,
+                              :service_code => service_code,
+                              :packages => packages,
+                              :delivery_range => [delivery_date])
+        end
       end
-      #RateResponse.new(success, message, Hash.from_xml(response).values.first, :rates => rate_estimates, :xml => response, :request => last_request)
-      return 'Feature Not Available'
+      RateResponse.new(success, message, Hash.from_xml(response).values.first, :rates => rate_estimates, :xml => response, :request => last_request)
     end
     
     def parse_tracking_response(response, options={})
