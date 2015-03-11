a8fddde4f3d5c594f72376ae77c05b2a7329f70a
Donavan White <digi.cazter@gmail.com>
2015-03-11 13:42:32 -0400

    Fix

--- lib/omniship.rb
+++ lib/omniship.rb
@@ -51,10 +51,12 @@ $:.unshift File.dirname(__FILE__)
 
 begin
   require 'active_support/all'
+  require 'active_merchant'
 rescue LoadError => e
   require 'rubygems'
   gem "activesupport", ">= 2.3.5"
   require "active_support/all"
+  require 'active_merchant'
 end
 
 autoload :XmlNode, 'vendor/xml_node/lib/xml_node'
